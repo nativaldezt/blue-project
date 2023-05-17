11df9e934772648cbef7234d02ae1255b8f8b1ae
Natalia Valdez <natalia@MacBook-Pro-de-Natalia.local>
2023-05-16 23:17:45 -0400

    Base code.

---
+++ src/test/java/com/nvaldez/blue/SpringContextLiveTest.java
@@ -0,0 +1,13 @@
+package com.nvaldez.blue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringContextLiveTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
